cc901df4314ddf67cf9417e735e19bb5cd2d4650
João Silva <jony.silva2013@gmail.com>
2025-12-14 00:34:44 +0000

    Made the change to Nginx

--- code/jvm/PokerDice/app/src/main/kotlin/pt/isel/WebApp.kt
+++ code/jvm/PokerDice/app/src/main/kotlin/pt/isel/WebApp.kt
@@ -34,7 +34,7 @@ class PipelineConfigurer(
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("http://localhost:5173", "http://localhost:4000")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
